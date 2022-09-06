a63c7a304a3a26b31dfaa3d4781cd5379847307d
09LittleUncle <51184871+09LittleUncle@users.noreply.github.com>
2022-09-06 16:06:24 +0800

    Add files via upload

---
+++ Prime_Number.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;	//Java输入
+
+public class Prime_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Java输入
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入一个整数：");
+        int number = sc.nextInt();
+		
+		find(number);
+	}
+	
+	public static int zhishu(int x) {
+		int flag = 1;
+		for(int i = 2; i < x; i++) {
+			if(x%i == 0) {
+				flag = 0;
+				break;
+			}
+		}
+		if(flag == 1)	return x;
+		else 			return 0;
+	}
+	
+	public static void find(int n) {
+		if(zhishu(n) != 0) {
+			System.out.println(n);
+		}
+		else {
+			int i;
+			for(i = 2; zhishu(i) == 0 || n % i != 0; i++) ;
+			System.out.print(i+"*");
+			find(n/i);
+		}
+	}
+}
