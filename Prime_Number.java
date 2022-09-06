9d588f66349feae3bfb9036441bffc76cfeb4e29
09LittleUncle <51184871+09LittleUncle@users.noreply.github.com>
2022-09-06 16:11:57 +0800

    Add files via upload

--- Prime_Number.java
+++ Prime_Number.java
@@ -5,7 +5,7 @@ public class Prime_Number {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Java输入
+		//分解质因数
 		Scanner sc = new Scanner(System.in);
 		System.out.print("请输入一个整数：");
         int number = sc.nextInt();
@@ -27,13 +27,13 @@ public class Prime_Number {
 	
 	public static void find(int n) {
 		if(zhishu(n) != 0) {
-			System.out.println(n);
+			System.out.println(n);		//如果其本身就是质数，则直接输出
 		}
 		else {
 			int i;
-			for(i = 2; zhishu(i) == 0 || n % i != 0; i++) ;
+			for(i = 2; zhishu(i) == 0 || n % i != 0; i++) ;	//从2开始寻找一个 既是质数又可以将该整数整除的整数i。
 			System.out.print(i+"*");
-			find(n/i);
+			find(n/i);					//将该整数分解
 		}
 	}
 }
